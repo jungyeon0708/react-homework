260c0baad173a1c1411eb5c8fc805117bb5ca5a8
jungyeon0708 <kimkjy0708@gmail.com>
2025-08-20 18:50:51 +0900

    feat: week2 상태있는 컴포넌트 과제를 위한 파일 구조 추가
    - SignUpForm, SignInForm 컴포넌트 파일 생성
    - 상태 관리 컴포넌트 과제 진행을 위한 폴더 구조 설정
    - React useState 실습을 위한 기본 틀 준비

--- week2/src/main.jsx
+++ week2/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './demo/app.jsx'
+import App from './components/index.jsx'
 import './styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('container')).render(
