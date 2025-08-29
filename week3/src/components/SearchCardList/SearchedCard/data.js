d621a04c8d99776436c44b12c5366f53d802b44a
jungyeon0708 <kimkjy0708@gmail.com>
2025-08-29 21:07:44 +0900

    feat : 캐릭터 카드 데이터 생성

---
+++ week3/src/components/SearchCardList/SearchedCard/data.js
@@ -0,0 +1,164 @@
+// 캐릭터 카드 더미 데이터
+export const cardData = [
+  {
+    id: 1,
+    name: 'Hello Kitty',
+    hanName: '헬로키티',
+    tags: [
+      { name: 'baked', image: '/public/assets/HelloKitty/Baked_Icon.webp' },
+      { name: 'fancy', image: '/public/assets/HelloKitty/Fancy_Icon.webp' },
+      { name: 'fruity', image: '/public/assets/HelloKitty/Fruity_Icon.webp' },
+    ],
+    image: '/public/assets/HelloKitty/Hello_Kitty_Icon.webp',
+  },
+  {
+    id: 2,
+    name: 'Cinnamoroll',
+    hanName: '시나모롤',
+    tags: [
+      {
+        name: 'chocolate',
+        image: '/public/assets/Chinamoroll/Chocolate_Icon_2.webp',
+      },
+      { name: 'coffee', image: '/public/assets/Chinamoroll/Coffee_Icon.webp' },
+      { name: 'spicy', image: '/public/assets/Chinamoroll/Spicy_Icon.webp' },
+    ],
+    image: '/public/assets/Chinamoroll/Cinnamoroll_Icon.webp',
+  },
+  {
+    id: 3,
+    name: 'Kuromi',
+    hanName: '쿠로미',
+    tags: [
+      { name: 'fall', image: '/public/assets/Kuromi/Fall_Icon.webp' },
+      { name: 'soda', image: '/public/assets/Kuromi/Soda_Icon.webp' },
+      { name: 'spooky', image: '/public/assets/Kuromi/Spooky_Icon.webp' },
+    ],
+    image: '/public/assets/Kuromi/Kuromi_Icon.webp',
+  },
+  {
+    id: 4,
+    name: 'Keroppi',
+    hanName: '케로피',
+    tags: [
+      { name: 'critter', image: '/public/assets/Keroppi/Critter_Icon.webp' },
+      { name: 'swamp', image: '/public/assets/Keroppi/Swamp_Icon.webp' },
+      { name: 'wood', image: '/public/assets/Keroppi/Wood_Icon.webp' },
+    ],
+    image: '/public/assets/Keroppi/Keroppi_Icon.webp',
+  },
+  {
+    id: 5,
+    name: 'Pompompurin',
+    hanName: '폼폼푸린',
+    tags: [
+      { name: 'calm', image: '/public/assets/Pompompurin/Calm_Icon_2.webp' },
+      {
+        name: 'dessert',
+        image: '/public/assets/Pompompurin/Dessert_Icon.webp',
+      },
+      { name: 'milk', image: '/public/assets/Pompompurin/Milk_Icon.webp' },
+    ],
+    image: '/public/assets/Pompompurin/Pompompurin_Icon.webp',
+  },
+  {
+    id: 6,
+    name: 'Chococat',
+    hanName: '초코캣',
+    tags: [
+      { name: 'book', image: '/public/assets/Chococat/Book_Icon.webp' },
+      {
+        name: 'chocolate',
+        image: '/public/assets/Chococat/Chocolate_Icon.webp',
+      },
+      {
+        name: 'mechanical',
+        image: '/public/assets/Chococat/Mechanical_Icon.webp',
+      },
+    ],
+    image: '/public/assets/Chococat/Chococat_Icon.webp',
+  },
+  {
+    id: 7,
+    name: 'Badtz-Maru',
+    hanName: '바츠마루',
+    tags: [
+      { name: 'joke', image: '/public/assets/BadtzMaru/Joke_Icon.webp' },
+      { name: 'pizza', image: '/public/assets/BadtzMaru/Pizza_Icon.webp' },
+      {
+        name: 'tropical',
+        image: '/public/assets/BadtzMaru/Tropical_Icon_2.webp',
+      },
+    ],
+    image: '/public/assets/BadtzMaru/Badtz-maru_Icon.webp',
+  },
+  {
+    id: 8,
+    name: 'Hangyodon',
+    hanName: '한교동',
+    tags: [
+      { name: 'aquatic', image: '/public/assets/Hangyodong/Aquatic_Icon.webp' },
+      { name: 'fabric', image: '/public/assets/Hangyodong/Fabric_Icon.webp' },
+      { name: 'fish', image: '/public/assets/Hangyodong/Fish_Icon.webp' },
+    ],
+    image: '/public/assets/Hangyodong/Hangyodon_Icon.webp',
+  },
+  {
+    id: 9,
+    name: 'My Melody',
+    hanName: '마이멜로디',
+    tags: [
+      { name: 'pink', image: '/public/assets/MyMelody/Pink_Icon.webp' },
+      { name: 'sleepy', image: '/public/assets/MyMelody/Sleepy_Icon.webp' },
+      { name: 'sweet', image: '/public/assets/MyMelody/Sweet_Icon.webp' },
+    ],
+    image: '/public/assets/MyMelody/My_Melody_Icon.webp',
+  },
+  {
+    id: 10,
+    name: 'Pekkle',
+    hanName: '페클',
+    tags: [
+      { name: 'calm', image: '/public/assets/Pekkle/Calm_Icon.webp' },
+      { name: 'mountain', image: '/public/assets/Pekkle/Mountain_Icon.webp' },
+      { name: 'musical', image: '/public/assets/Pekkle/Musical_Icon.webp' },
+    ],
+    image: '/public/assets/Pekkle/Pekkle_Icon.webp',
+  },
+  {
+    id: 11,
+    name: 'Pochacco',
+    hanName: '포차코',
+    tags: [
+      { name: 'healthy', image: '/public/assets/Pochacco/Healthy_Icon.webp' },
+      { name: 'trophy', image: '/public/assets/Pochacco/Trophy_Icon.webp' },
+      { name: 'veggie', image: '/public/assets/Pochacco/Veggie_Icon.webp' },
+    ],
+    image: '/public/assets/Pochacco/Pochacco_Icon.webp',
+  },
+  {
+    id: 12,
+    name: 'Retsuko',
+    hanName: '레츠코',
+    tags: [
+      { name: 'fire', image: '/public/assets/Retsuko/Fire_Icon.webp' },
+      { name: 'ingot', image: '/public/assets/Retsuko/Ingot_Icon.webp' },
+      { name: 'musical', image: '/public/assets/Retsuko/Musical_Icon_2.webp' },
+    ],
+    image: '/public/assets/Retsuko/Retsuko_Icon.webp',
+  },
+  {
+    id: 13,
+    name: 'Tuxedosam',
+    hanName: '턱시도샘',
+    tags: [
+      { name: 'fabric', image: '/public/assets/Tuxedosam/Fabric_Icon_2.webp' },
+      { name: 'fancy', image: '/public/assets/Tuxedosam/Fancy_Icon_2.webp' },
+      {
+        name: 'tropical',
+        image: '/public/assets/Tuxedosam/Tropical_Icon.webp',
+      },
+    ],
+    image: '/public/assets/Tuxedosam/Tuxedosam_Icon.webp',
+  },
+]
